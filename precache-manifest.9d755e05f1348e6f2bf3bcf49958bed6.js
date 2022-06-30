288f0e8c27dae048d1047775197950f0e6a508b0
Karan Kalra <karan@nanonets.com>
2022-06-30 14:15:56 +0530

    Updates

---
+++ precache-manifest.9d755e05f1348e6f2bf3bcf49958bed6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f852ffd4c2050ca9988",
+    "url": "/static/css/main.fc841192.chunk.css"
+  },
+  {
+    "revision": "0f852ffd4c2050ca9988",
+    "url": "/static/js/main.0f852ffd.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f40bae1a3d90ec68f786",
+    "url": "/static/js/2.f40bae1a.chunk.js"
+  },
+  {
+    "revision": "732a198f1827dc0cb79ad213b28d97df",
+    "url": "/index.html"
+  }
+];
